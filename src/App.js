586fd5cf7f4d405ece5edb13c5b4b591a187d3b4
Elysiayn <wendy.lemus.wl@gmail.com>
2021-03-20 09:49:16 -0400

    imported materialize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import Nav from './components/Nav';
+import Navbar from './components/Navbar';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
+import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
   const [currentPage, handlePageChange] = useState('About');
@@ -30,7 +31,7 @@ function App() {
 
   return (
     <div>
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
       <div>
         {
           // Render the component returned by 'renderPage()'
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,59 @@
+import React, {Component } from 'react';
+// Import Materialize
+import 'materialize-css/dist/css/materialize.min.css';
+import M from "materialize-css/dist/js/materialize.min.js";
+
+export class Navbar extends Component{
+  
+  componentDidMount(){
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, {});
+    });
+  
+    // M.AutoInit();
+  }
+
+  // const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
+  render() {
+    return (
+      <div>
+        <nav>
+          <div class="nav-wrapper blue">
+
+          <a href="#" data-target="slide-out" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i></a>
+
+            <div className="container">
+              <a href="#" class="brand-logo">Elysiayn</a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="about">About Me</a></li>
+                <li><a href="portfolio">Portfolio</a></li>
+                <li><a href="contact">Contact</a></li>
+                <li><a href="resume">Resume</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        
+        <ul id="slide-out" class="sidenav">
+          <li><div class="user-view">
+            <div class="background">
+              <img src="images/office.jpg"/>
+            </div>
+            <a href="#user"><img class="circle" src={require(`../../assets/images/pic_crop.jpg`)}/></a>
+            <a href="#name"><span class="grey-text name">Wendy Lemus</span></a>
+            <a href="#email"><span class="grey-text email">Elysiayn@gmail.com</span></a>
+          </div></li>
+          <li><div class="divider"></div></li>
+          <li><a href="#!"><i class="material-icons">cloud</i>About Me</a></li>
+          <li><a href="#!"><i class="material-icons">cloud</i>Portfolio</a></li>
+          <li><a href="#!"><i class="material-icons">cloud</i>Contact</a></li>
+          <li><a href="#!"><i class="material-icons">cloud</i>Resume</a></li>
+          <li><a class="subheader">Subheader</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
