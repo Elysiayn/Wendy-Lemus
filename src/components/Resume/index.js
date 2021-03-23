84d54c93ce2a5167eaad489855a4a0ad6c461169
Elysiayn <wendy.lemus.wl@gmail.com>
2021-03-23 08:17:15 -0400

    added styling to resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 const Resume = () => (
-  <div>
-    <h1>Resume Page</h1>
-    <p>
-      RESUME TEXT
-    </p>
+  <div className="row about resume">
+  <div  className="col s12 m12 l12 ">
+    <div className="card aboutCard ">
+
+      <div className="card-content">
+      <h1 data-testid="h1tag">Resume</h1>
+      <h4 classNAme="flow-text">Skills:</h4>
+        <p className="aboutText flow-text">
+        * HTML, CSS, Git, JavaScript, BootStrap, Materialize, jQuery, REST API, Node.js,
+        Express.js, MongoDB, Mongoose, MySQLS, Sequelize, Object Oriented Programming and more!
+        </p>
+        <div class="card-action center teal darken-2">
+          <a href="#">My full resume!</a>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 );
 
 export default Resume;
