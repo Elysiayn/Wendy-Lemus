1699f36b1101d5fcdb8fb7419f5890a9e7458995
Elysiayn <wendy.lemus.wl@gmail.com>
2021-04-05 12:22:58 -0400

    updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resumeFile from '../../assets/images/LemusResume.pdf';
+import resumeFile from '../../assets/images/Resume.pdf';
 
 const Resume = () => (
   <div className="row about resume">
@@ -8,10 +8,21 @@ const Resume = () => (
 
       <div className="card-content">
       <h1 data-testid="h1tag">Resume</h1>
-      <h4 classNAme="flow-text">Skills:</h4>
+      <h4 className="flow-text">Languages:</h4>
         <p className="aboutText flow-text">
-        * HTML, CSS, Git, JavaScript, BootStrap, Materialize, jQuery, REST API, Node.js,
-        Express.js, MongoDB, Mongoose, MySQL, Sequelize, Object Oriented Programming and more!
+        * JavaScript, HTML5, CSS, JSON
+        </p>
+      <h4 className="flow-text">Database:</h4>
+        <p className="aboutText flow-text">
+        * MySQL, MongoDB
+        </p>
+      <h4 className="flow-text">Tools:</h4>
+        <p className="aboutText flow-text">
+        * Sequelize, SQLite, Express.js, jQuery, FireBase Storage, GraphQL, Mongoose, Node.js, React, Regex, Stripe, Redux
+        </p>
+      <h4 className="flow-text">Other:</h4>
+        <p className="aboutText flow-text">
+        * Heroku, Git Bash, GitHub, Microsoft Office
         </p>
         <div class="card-action center teal darken-2">
         <a href={resumeFile} download={resumeFile}>Download Resume</a>
